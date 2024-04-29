ac5df0c2515634891919248be0e67f17593e1a92
Bushmeen <bushmeen@gmail.com>
2024-04-29 10:50:40 +0200

    add domain to api

--- lib/api.ts
+++ lib/api.ts
@@ -1,13 +1,10 @@
 import { Project } from '@/types/types';
 import { notFound } from 'next/navigation';
 
-export const domain =
-	process.env.NODE_ENV === 'production' ? 'https://sepetowski.vercel.app' : 'http://localhost:3000';
-
 export const getProject = async (locale: string, projectName: string) => {
 	try {
 		const res = await fetch(
-			`${domain}/api/getProjectInfo?locale=${locale}&projectName=${projectName}`,
+			`https://sepetowski.vercel.app/api/getProjectInfo?locale=${locale}&projectName=${projectName}`,
 			{
 				method: 'GET',
 			}
